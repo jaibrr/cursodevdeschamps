935e88bfabff7d131d108010b7e7b62597faa7bc
jaibrr <jainabbarreto@gmail.com>
2026-02-05 08:25:40 -0300

    update Home component to display library organization message

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Meu benzinho, estou testando uns bagulhos</h1>;
+  return (
+    <h1>
+      Você sabe quantos e quais livros você tem? Aqui será construído um lugar
+      em que você pode organizar sua biblioteca virtualmente!
+    </h1>
+  );
 }
 
 export default Home;
